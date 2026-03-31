b28ad5ffc16cc22507b6fb7ce4e1060dd33b9099
vk57434 <vk4492020@gmail.com>
2026-03-31 23:44:50 +0530

    update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import adminRoutes from "./routes/adminRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
