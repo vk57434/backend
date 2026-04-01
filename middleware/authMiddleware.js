7926d1a0302d30b170f9eac14fe9f0ea69003761
vk57434 <vk4492020@gmail.com>
2026-04-01 19:06:10 +0530

    fix middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,20 @@
 import jwt from "jsonwebtoken";
 
 export const protect = (req, res, next) => {
-  const token = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  if (!token) return res.status(401).json({ msg: "No token" });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ msg: "No token" });
+  }
 
   try {
+    const token = authHeader.split(" ")[1]; 
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
+
     next();
-  } catch {
+  } catch (error) {
     res.status(401).json({ msg: "Invalid token" });
   }
 };
